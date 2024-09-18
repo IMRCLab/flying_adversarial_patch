566f33354930d854f9ce3117b6e6345322957bef
phanfeld <pia.hanfeld@gmail.com>
2024-09-18 13:49:21 +0200

    changed paths and functions according to refactor, untested

--- diffusion/eval_diffusion.py
+++ diffusion/eval_diffusion.py
@@ -3,50 +3,56 @@ import torch.nn as nn
 import numpy as np
 
 from diffusion.diffusion_model import DiffusionModel
-from simulators.cf_frontnet_pt import CFSim
+# from simulators.cf_frontnet_pt import CFSim
 
-from eval_gt_patches import calc_loss, get_targets, gen_T
+# from eval_gt_patches import calc_loss, get_targets, gen_T
+from src.attacks import calc_anytime_loss
 
-def loss_dataset(gt_patch, diffusion_patch, random_patch, ft_patch, targets, position, dataset, sim):
-    T = gen_T(position)
+def loss_dataset(model, gt_patch, diffusion_patch, random_patch, ft_patch, targets, positions_gt, positions_ft, dataset):
+    # T = gen_T(position)
 
-    gt_losses = []
-    diffusion_losses = []
-    random_losses = []
-    ft_losses = []
+    # gt_losses = []
+    # diffusion_losses = []
+    # random_losses = []
+    # ft_losses = []
 
-    for data in dataset:
-        img, _ = data
+    # for data in dataset:
+    #     img, _ = data
+        _, gt_loss = calc_anytime_loss(0, dataset, gt_patch, targets, model, positions_gt, scale_min=0.2, scale_max=0.7, quantized=False)
+        # gt_losses.append(calc_loss([targets], gt_patch, [T], img, sim))
+        _, diffusion_loss = calc_anytime_loss(0, dataset, diffusion_patch, targets, model, positions_gt, scale_min=0.2, scale_max=0.7, quantized=False)
+        # diffusion_losses.append(calc_loss([targets], diffusion_patch, [T], img, sim))
+        _, random_loss = calc_anytime_loss(0, dataset, random_patch, targets, model, positions_gt, scale_min=0.2, scale_max=0.7, quantized=False)
+        # random_losses.append(calc_loss([targets], random_patch, [T], img, sim))
+        _, ft_gt_loss = calc_anytime_loss(0, dataset, ft_patch, targets, model, positions_gt, scale_min=0.2, scale_max=0.7, quantized=False)
+        # ft_losses.append(calc_loss([targets], ft_patch, [T], img, sim))
+        _, ft_ft_loss = calc_anytime_loss(0, dataset, ft_patch, targets, model, positions_ft, scale_min=0.2, scale_max=0.7, quantized=False)
 
-        gt_losses.append(calc_loss([targets], gt_patch, [T], img, sim))
-        diffusion_losses.append(calc_loss([targets], diffusion_patch, [T], img, sim))
-        random_losses.append(calc_loss([targets], random_patch, [T], img, sim))
-        ft_losses.append(calc_loss([targets], ft_patch, [T], img, sim))
+    # return np.array(gt_losses), np.array(diffusion_losses), np.array(random_losses), np.array(ft_losses)
+    return gt_loss.detach().cpu().numpy(), diffusion_loss.detach().cpu().numpy(), random_loss.detach().cpu().numpy(), ft_gt_loss.detach().cpu().numpy(), ft_ft_loss.detach().cpu().numpy()
 
-    return np.array(gt_losses), np.array(diffusion_losses), np.array(random_losses), np.array(ft_losses)
-
-def load_gt_dataset(path):
+def load_dataset(path):
     with open(path, 'rb') as f:
         data = pickle.load(f)
 
-    gt_patches = []
-    gt_targets = []
-    gt_positions = []
+    patches = []
+    targets = []
+    positions = []
     for i in range(len(data)):
-        gt_patches.append(data[i][0])
-        gt_targets.append(data[i][1])
-        gt_positions.append(data[i][2])
+        patches.append(data[i][0])
+        targets.append(data[i][1])
+        positions.append(data[i][2])
 
-    return np.array(gt_patches)*255., np.array(gt_targets), np.array(gt_positions)
+    return np.array(patches)*255., np.array(targets), np.array(positions)
 
-def load_model(path, device):
-    model = UNet(in_size=1, out_size=1, device=device)
-    model.to(device)
+# def load_model(path, device):
+#     model = UNet(in_size=1, out_size=1, device=device)
+#     model.to(device)
 
-    model.load_state_dict(torch.load(path, map_location=device))
-    model.eval()
+#     model.load_state_dict(torch.load(path, map_location=device))
+#     model.eval()
 
-    return model
+#     return model
 
 
 if __name__ == '__main__':
@@ -59,29 +65,30 @@ if __name__ == '__main__':
 
     n_samples = 100
 
-    # model = load_model('conditioned_unet_80x80_1000_3256i_255.pth')
+    model = DiffusionModel(device)
+    model.load('/home/hanfeld/bb_FAP/conditioned_unet_80x80_1000_3256i_255.pth')
+
     
-    # random_idx = np.random.choice(len(gt_patches), size=n_samples, replace=False)
-    # np.save('/home/hanfeld/flying_adversarial_patch/results/fine-tuning80x80/indices.npy', random_idx)
-    # # random_idx = np.load('/home/hanfeld/flying_adversarial_patch/results/fine-tuning80x80/indices.npy')
+    random_idx = np.random.choice(len(gt_patches), size=n_samples, replace=False)
+    np.save('results/fine-tuning80x80/indices.npy', random_idx)
 
-    # gt_patches, gt_targets, gt_positions = load_gt_dataset('data/FAP_combined.pickle')
-    # gt_patches = gt_patches[random_idx]
-    # gt_targets = gt_targets[random_idx]
-    # gt_positions = gt_positions[random_idx]
-
+    gt_patches, gt_targets, gt_positions = load_dataset('diffusion/FAP_combined.pickle')
+    gt_patches = gt_patches[random_idx]
+    gt_targets = gt_targets[random_idx]
+    gt_positions = gt_positions[random_idx]
 
 
-    # with torch.no_grad():
-    #     diffusion_patches = sample(model, torch.tensor(gt_targets), device, [80, 80], n_samples=n_samples, n_steps=1_000).detach().cpu()
+
+    with torch.no_grad():
+        diffusion_patches = model.sample(n_samples, torch.tensor(gt_targets), device, patch_size=[80,80], n_steps=1_000).detach().cpu()
 
     # # # # print(diffusion_patches.shape)
     # # # # # print(torch.min(diffusion_patches), torch.max(diffusion_patches))
     # diffusion_patches = (diffusion_patches - torch.min(diffusion_patches)) / (torch.max(diffusion_patches) - torch.min(diffusion_patches)) # normalize
-    # diffusion_patches *= 255.
+    diffusion_patches *= 255.
 
     # print(diffusion_patches.shape)
-    # diffusion_patches = diffusion_patches.detach().cpu().numpy()
+    diffusion_patches = diffusion_patches.detach().cpu().numpy()
     # np.save('eval/diffusion_patches_100.npy', diffusion_patches)
     # # # print(torch.min(diffusion_patches), torch.max(diffusion_patches))
     # # # print(diffusion_patches.shape)
@@ -89,7 +96,7 @@ if __name__ == '__main__':
     # diffusion_patches = torch.tensor(np.load('eval/diffusion_patches_100.npy'))
     # # print(diffusion_patches.shape)
 
-    # # # np.save(f'eval/diffusion_patches_{n_samples}.npy', diffusion_patches.numpy())
+    np.save(f'eval/diffusion_patches_{n_samples}.npy', diffusion_patches.numpy())
 
     # # # import matplotlib.pyplot as plt
     # # # fig = plt.figure(constrained_layout=True)
@@ -105,75 +112,31 @@ if __name__ == '__main__':
 
     # sim = CFSim()
     
-    # # # generate random patches
-    # random_patches = torch.rand_like(diffusion_patches) * 255.
-    # random_patches = torch.rand(n_samples, 80, 80) * 255.
-    # print(random_patches.shape, random_patches.max())
+    # generate random patches
+    random_patches = torch.rand_like(diffusion_patches) * 255.
+    random_patches = torch.rand(n_samples, 80, 80) * 255.
+    print(random_patches.shape, random_patches.max())
+
+    ft_patches, ft_targets, ft_positions = load_dataset('diffusion/FAP_fine_tuning.pickle')
 
 
-    # with open('data/FAP_fine_tuning.pickle', 'rb') as f:
-    #     data = pickle.load(f)
-    # ft_patches = []
-    # ft_targets = []
-    # ft_positions = []
-    # for i in range(len(data)):
-    #     ft_patches.append(data[i][0])
-    #     ft_targets.append(data[i][1])
-    #     ft_positions.append(data[i][2])
+    all_gt, all_diffusion, all_random, all_ft_at_gt, all_ft_at_ft = []
 
-    # ft_patches = np.array(ft_patches) * 255.
-    # ft_targets = np.array(ft_targets)
-    # ft_positions = np.array(ft_positions)
+    from tqdm import trange
 
-    # print(ft_patches.shape)
-    # print(ft_targets.shape)
-    # print(ft_positions.shape)
+    for i in trange(n_samples):
+        gt_loss, diffusion_loss, random_loss, ft_at_gt_loss, ft_at_ft_loss = loss_dataset(model, gt_patches, diffusion_patch, random_patch, ft_patch, targets, positions_gt, positions_ft, dataset, sim)
+        all_gt.append(gt_loss)
+        all_diffusion.append(diffusion_loss)
+        all_random.append(random_loss)
+        all_ft_at_gt.append(ft_at_gt_loss)
+        all_ft_at_ft.append(ft_at_ft_loss)
 
-    # dataset_path = "/home/hanfeld/flying_adversarial_patch/pulp-frontnet/PyTorch/Data/160x96StrangersTestset.pickle"
-    # _, dataset = sim.load_dataset(dataset_path, batch_size=1, train_set_size=0.9)
-
-    # # # print(len(dataset))
-
-    # from tqdm import tqdm
-
-    # all_gt = []
-    # all_diffusion = []
-    # all_random = []
-    # all_ft = []
-    # for idx, (gt_patch, diffusion_patch, random_patch, ft_patch) in enumerate(tqdm(zip(torch.tensor(gt_patches), diffusion_patches, random_patches, torch.tensor(ft_patches)))):
-    #     gt_loss, diffusion_loss, _, ft_loss = loss_dataset(gt_patch, diffusion_patch[0], random_patch[0], ft_patch, torch.tensor(gt_targets[idx]), gt_positions[idx], dataset, sim)
-    #     all_gt.append(gt_loss)
-    #     all_diffusion.append(diffusion_loss)
-    #     # all_random.append(random_loss)
-    #     all_ft.append(ft_loss)
-    # all_ft_at_gt = []
-    # for idx, rnd_patch in enumerate(tqdm(random_patches)):
-    #     _, _, random_loss, _ = loss_dataset([], [], rnd_patch, [], torch.tensor(gt_targets[idx]), gt_positions[idx], dataset, sim)
-        # all_gt.append(gt_loss)
-        # all_diffusion.append(diffusion_loss)
-        # all_random.append(random_loss)
-        # all_ft_at_gt.append(ft_loss)
-
-    # all_ft_at_ft = []
-    # for idx, ft_patch in enumerate(tqdm(torch.tensor(ft_patches))):
-    #     _, _, _, ft_loss = loss_dataset([], [], [], ft_patch, torch.tensor(gt_targets[idx]), ft_positions[idx], dataset, sim)
-        # all_gt.append(gt_loss)
-        # all_diffusion.append(diffusion_loss)
-        # all_random.append(random_loss)
-    #     all_ft_at_ft.append(ft_loss)
-        
-    # all_ft_at_gt = np.array(all_ft_at_gt)
-    # all_ft_at_ft = np.array(all_ft_at_ft)
-
-    # all_gt = np.array(all_gt)
-    # all_diffusion = np.array(all_diffusion)
-    # all_random = np.array(all_random)
-    # all_ft = np.array(all_ft)
-
-    # print(all_gt.shape)
-    # print(all_diffusion.shape)
-    # print(all_random.shape)
-    # print(all_ft.shape)
+    all_gt = np.array(all_gt)
+    all_diffusion = np.array(all_diffusion)
+    all_random = np.array(all_random)
+    all_ft_at_gt = np.array(all_ft_at_gt)
+    all_ft_at_ft = np.array(all_ft_at_ft)
 
     # np.save(f'eval/comparison_gt_{n_samples}.npy', all_gt)
     # np.save(f'eval/comparison_diffusion_{n_samples}.npy', all_diffusion)
