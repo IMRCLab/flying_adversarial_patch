cfa22df4be707a9282ffaf71cabd15ef4fe01289
phanfeld <pia.hanfeld@gmail.com>
2025-01-27 11:57:39 +0100

    prediction on multiple images not equal to prediction on single image

--- src/dji_yolo_test.py
+++ src/dji_yolo_test.py
@@ -6,6 +6,10 @@ import glob
 
 import pickle
 
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
 # taken from ultralytics yolo
 def xywh2xyxy(x):
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
@@ -25,6 +29,19 @@ def scale_box(box, scale_factor_width, scale_factor_height):
 
 SOFTMAX_SCALE = 20.
 
+def create_pkl(path, file_type='jpg'):
+    # load all images from images_folder
+    all_images = []
+    for file in tqdm(sorted(glob.glob(f'{path}/*.{file_type}'))):
+        img = cv2.imread(file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (640, 320))
+        #print(img.shape)
+        all_images.append(img.transpose(2, 0, 1)) # shape: (3, 320, 640)
+
+    all_images = np.array(all_images)
+    with open('misc/dji/all_resized_images.pkl', 'wb') as f:
+        pickle.dump(all_images, f)
 
 if __name__ == '__main__':
   
@@ -34,21 +51,36 @@ if __name__ == '__main__':
     model = torch.hub.load("ultralytics/yolov5", "yolov5n", pretrained=True, autoshape=False)
     model.eval()
 
+    scale_factor_height = 320 / 2160
+    scale_factor_width = 640 / 3840
+
     # load image from path with cv2
     images_folder = 'misc/dji/images'
-    output_folder = 'misc/dji/bb_predictions'
+    output_folder = 'misc/dji/temp_batch_prediction'
     os.makedirs(output_folder, exist_ok=True)
+
+
+    # create_pkl(images_folder)
+    #plt.imsave('misc/dji/temp_load.jpg', all_images[0].transpose(1, 2, 0))
+
 
     # load images from pickle
     with open('misc/dji/all_resized_images.pkl', 'rb') as f:
-        images = np.array(pickle.load(f))   # shape: (num_images, 320, 640, 3)
+        images = np.array(pickle.load(f))   # shape: (num_images, 3, 320, 640)
     
-    # print(images.shape)
-    # permute to (num_images, 3, 320, 640)
-    images = images.transpose(0, 3, 1, 2)
     print(images.shape)
+    print(images[0].min(), images[0].max())
+    # # permute to (num_images, 3, 320, 640)
+    # images = images.transpose(0, 3, 1, 2)
+    # print(images.shape)
+    images = torch.tensor(images/255.).float()
+    print(images.shape)
+    print(images[0].min(), images[0].max())
 
-    dataloader = torch.utils.data.DataLoader(images / 255., batch_size=32, shuffle=False)
+    # # plot image with matplotlib
+    # plt.imsave('misc/dji/temp.jpg', images[0].permute(1, 2, 0).detach().cpu().numpy())
+
+    dataloader = torch.utils.data.DataLoader(images, batch_size=8, shuffle=True)
 
     batch = next(iter(dataloader))
     print(batch.shape)
@@ -57,12 +89,72 @@ if __name__ == '__main__':
     results = model(batch.to(device))[0]
     print(results.shape) 
 
+    boxes = xywh2xyxy(results[:, :, :4])
+    print(boxes.shape)
+    scores = results[:, :, 4] * results[0][:, 5]
+    print(scores.shape)
+
+    # scores prediction for each box
+    soft_scores_batches = torch.nn.functional.softmax(scores * SOFTMAX_SCALE, dim=1)
+    print(soft_scores_batches.shape)
+    selected_box = torch.bmm(soft_scores_batches.unsqueeze(1), boxes).squeeze(1).detach().cpu().numpy()
+    print(selected_box.shape)
+
+    # # max score prediction
+    # max_scores_idx = torch.argmax(scores, dim=1).detach().cpu().numpy()
+    # max_boxes = np.array([boxes[i][idx].detach().cpu().numpy() for i, idx in enumerate(max_scores_idx)])
+    # print(max_boxes.shape)
+
+    for counter, img in enumerate(batch):
+        img = img.permute(1, 2, 0).detach().cpu().numpy()
+        img = (img * 255).astype(np.uint8)
+        print(img.shape)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # scale image to height = 640
+
+
+        # draw box
+        #scaled_box = scale_box(selected_box[counter], scale_factor_width, scale_factor_height)
+        xmin, ymin, xmax, ymax = selected_box[counter]
+        cv2.rectangle(img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 10)
+        #cv2.rectangle(img, (int(max_boxes[counter][0]), int(max_boxes[counter][1]), int(max_boxes[counter][2]), int(max_boxes[counter][3])), (255, 0, 0), 10)
+        cv2.imwrite(f'{output_folder}/batch_{counter:04d}.jpg', img)
+
+    
+    for counter, img in enumerate(batch):
+        img_t = img.unsqueeze(0).to(device)
+        result = model(img_t)[0]
+
+        boxes = xywh2xyxy(result[:, :, :4])
+        # print(boxes.shape)
+        scores = result[:, :, 4] * result[0][:, 5]
+        # print(scores.shape)
+
+        soft_scores = torch.nn.functional.softmax(scores * SOFTMAX_SCALE, dim=1)
+        difference = torch.mean((soft_scores - soft_scores_batches[counter])**2)
+        print(difference)
+        # print(soft_scores.shape)
+        selected_box = torch.bmm(soft_scores.unsqueeze(1), boxes).squeeze(1).detach().cpu().numpy()
+        # print(selected_box.shape)
+
+        img = img.permute(1, 2, 0).detach().cpu().numpy()
+        img = (img * 255).astype(np.uint8)
+        # print(img.shape)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # scale image to height = 640
+
+
+        # draw box
+        #scaled_box = scale_box(selected_box[counter], scale_factor_width, scale_factor_height)
+        xmin, ymin, xmax, ymax = selected_box[0]
+        cv2.rectangle(img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255), 10)
+        cv2.imwrite(f'{output_folder}/single_{counter:04d}.jpg', img)
 
 
 
     # all_boxes = []
 
-    # for counter, file in enumerate(sorted(glob.glob(f'{images_folder}/*.jpg'))):
+    #for counter, file in enumerate(sorted(glob.glob(f'{images_folder}/*.jpg'))):
     #     img = cv2.imread(file)
     #     cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     #     # scale image to height = 640
