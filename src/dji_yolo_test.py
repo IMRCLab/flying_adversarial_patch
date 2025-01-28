4b687ffbe1251aa55dd972f73c8b409dbafa1c84
phanfeld <pia.hanfeld@gmail.com>
2025-01-28 17:34:08 +0100

    slight refactor, start training from random target posiiton

--- src/dji_yolo_test.py
+++ src/dji_yolo_test.py
@@ -15,6 +15,12 @@ from patch_placement import place_patch
 
 import rowan
 
+import argparse
+from pathlib import Path
+
+
+SOFTMAX_SCALE = 20.
+
 # rotation from opencv frame to our coordinate frame
 # opencv: x -> right, y -> down, z -> forward
 # ours: x -> forward, y -> left, z -> up
@@ -39,8 +45,6 @@ def scale_box(box, scale_factor_width, scale_factor_height):
     box[2] /= scale_factor_width
     box[3] /= scale_factor_height
     return box
-
-SOFTMAX_SCALE = 20.
 
 def create_pkl(path, file_type='jpg'):
     # load all images from images_folder
@@ -140,6 +144,24 @@ def xyz_from_bb(bb, camera_intrinsic, radius =0.4):
         return xyz_imrc
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('results', default='results/dji/test', type=str, help='Path to save results')
+    parser.add_argument('--epochs', default=1000, type=int, help='Number of epochs')
+    parser.add_argument('--lr', default=1e-3, type=float, help='Learning rate')
+    parser.add_argument('--patch_size_width', default=80, type=int, help='Width of patch')
+    parser.add_argument('--patch_size_height', default=80, type=int, help='Height of patch')
+    parser.add_argument('--verbose', default=False, action='store_true')
+    args = parser.parse_args()
+
+    train_config = {'path': args.results, 
+                    'epochs': args.epochs, 
+                    'lr': args.lr, 
+                    'patch_size_width': args.patch_size_width, 
+                    'patch_size_height': args.patch_size_height, 
+                    'verbose': args.verbose}
+    
+    with open(f'{args.results}/train_config.yaml', 'w') as f:
+        yaml.dump(train_config, f)
   
   
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -158,11 +180,11 @@ if __name__ == '__main__':
     scale_max = 1.8
 
     # load image from path with cv2
-    images_folder = 'misc/dji/images'
-    output_folder = 'misc/dji/temp_batch_prediction'
+    #images_folder = 'misc/dji/images'
+    output_folder = Path(train_config['path'])
     os.makedirs(output_folder, exist_ok=True)
 
-    patch = torch.rand(1, 3, 80, 80).to(device).requires_grad_(True)
+    patch = torch.rand(1, 3, train_config['patch_size_height'], train_config['patch_size_width']).to(device).requires_grad_(True)
     # print(random_patch.min(), random_patch.max())
 
     # load camera intrinsic from yaml
@@ -174,32 +196,30 @@ if __name__ == '__main__':
     
 
     #target_box = torch.tensor([480, 125, 619, 241]).to(device)
-    target_position = torch.tensor([1.5, 0.0, 0.0]).to(device) # predicting person 1.5 meters away from camera
+    #target_position = torch.tensor([1.5, 0.0, 0.0]).to(device) # predicting person 1.5 meters away from camera
+    inside = False
+    while not inside:
+        target_x = torch.FloatTensor(1,).uniform_(0., 2.5)
+        target_y = torch.FloatTensor(1,).uniform_(-1., 1.)
+        target_z = torch.FloatTensor(1,).uniform_(-0.5, 0.5)
+        target_position = torch.hstack([target_x, target_y, target_z]).to(device)
+        target_in_image = camera_intrinsic @ rotate_vector(torch.inverse(T_imrc), target_position)
+        img_x = int(target_in_image[0] / target_in_image[2])
+        img_y = int(target_in_image[1] / target_in_image[2])
+        if img_x in range(0, 640) and img_y in range(0, 320):
+            inside = True
+    
     print(target_position.shape)
-    target_in_image = camera_intrinsic @ rotate_vector(torch.inverse(T_imrc), target_position)
-    target_x = int(target_in_image[0] / target_in_image[2])
-    target_y = int(target_in_image[1] / target_in_image[2])
-    print(target_x, target_y)
+    print(img_x, img_y)
 
-    # create_pkl(images_folder)
-    #plt.imsave('misc/dji/temp_load.jpg', all_images[0].transpose(1, 2, 0))
-
+    np.save(f'{output_folder}/target_position.npy', target_position.detach().cpu().numpy())
 
     # load images from pickle
     with open('misc/dji/all_resized_images.pkl', 'rb') as f:
         images = np.array(pickle.load(f))   # shape: (num_images, 3, 320, 640)
     
-    # print(images.shape)
-    # print(images[0].min(), images[0].max())
-    # # permute to (num_images, 3, 320, 640)
-    # images = images.transpose(0, 3, 1, 2)
-    # print(images.shape)
-    images = torch.tensor(images/255.).float()
-    # print(images.shape)
-    # print(images[0].min(), images[0].max())
 
-    # # plot image with matplotlib
-    # plt.imsave('misc/dji/temp.jpg', images[0].permute(1, 2, 0).detach().cpu().numpy())
+    images = torch.tensor(images/255.).float()
 
     dataloader = torch.utils.data.DataLoader(images, batch_size=32, shuffle=True)
 
@@ -209,33 +229,20 @@ if __name__ == '__main__':
     ty = torch.FloatTensor(1,).uniform_(-1., 1.).to(device).requires_grad_(True)
 
 
-    opt = torch.optim.Adam([patch, scale_factor, tx, ty], lr=1e-3)
+    opt = torch.optim.Adam([patch, scale_factor, tx, ty], lr=train_config['lr'])
 
     best_patch = None
     best_position = None
     best_loss = np.inf
     
-    for epoch in trange(20):
+    for epoch in trange(train_config['epochs']):
         epoch_losses = []
         for _, batch in enumerate(dataloader):
-
-            # print(scale_factor, tx, ty)
-            # print(sf_unnorm, tx_unnorm, ty_unnorm)
-
-            # sf_unnorm = inverse_norm(scale_factor, scale_min, scale_max)
-            # tx_unnorm = inverse_norm(tx, tx_min, tx_max)
-            # ty_unnorm = inverse_norm(ty, ty_min, ty_max)
-            
-            # transformation_matrix = get_transformation(sf_unnorm, tx_unnorm, ty_unnorm)
-            # print(transformation_matrix)
-            # print(transformation_matrix.shape)
 
             noisy_transformation = gen_noisy_transformations(batch.shape[0], scale_factor, tx, ty)
             # print(noisy_transformation.shape)
 
             mod_batch = place_patch(batch.to(device), patch.repeat(batch.shape[0], 1, 1, 1), noisy_transformation)
-            # print(mod_batch.shape)
-            # plt.imsave('misc/dji/temp_batch.jpg', mod_batch[0].permute(1, 2, 0).detach().cpu().numpy())
 
             results = model(mod_batch)[0]
             boxes_batch = xywh2xyxy(results[:, :, :4])
@@ -243,12 +250,11 @@ if __name__ == '__main__':
             soft_scores_batches = torch.nn.functional.softmax(scores_batch * SOFTMAX_SCALE, dim=1)
             
             selected_boxes = torch.bmm(soft_scores_batches.unsqueeze(1), boxes_batch).squeeze(1)
-            # print(selected_box.shape)
+
 
             # get xyz from bounding box
             xyz = torch.stack([xyz_from_bb(box, camera_intrinsic) for box in selected_boxes])
-            # print(xyz.shape)
-            # print(xyz[0])
+
 
             loss = torch.mean((xyz - target_position.repeat(batch.shape[0], 1))**2)
             epoch_losses.append(loss.detach().cpu().item())
@@ -264,12 +270,17 @@ if __name__ == '__main__':
         
         if np.mean(epoch_losses) < best_loss:
             best_patch = patch.detach().cpu().numpy()
-            best_position = np.hstack([scale_factor.detach().cpu().item(), tx.detach().cpu().item(), ty.detach().cpu().item()])
-            #print(best_position, best_position.shape)
+            unnorm_sf = inverse_norm(scale_factor.clone().detach(), scale_min, scale_max).cpu().item()
+            unnorm_tx = inverse_norm(tx.clone().detach(), tx_min, tx_max).cpu().item()
+            unnorm_ty = inverse_norm(ty.clone().detach(), ty_min, ty_max).cpu().item()
+            best_position = np.hstack([unnorm_sf, unnorm_tx, unnorm_ty])
+            # print(best_position, best_position.shape)
             best_loss = np.mean(epoch_losses)
+            np.save(f'{output_folder}/optim_patch.npy', best_patch)
+            np.save(f'{output_folder}/optim_transformation.npy', best_position)
 
 
-        if epoch % 10 == 0:
+        if args.verbose and epoch % 10 == 0:
             print(f'Epoch {epoch}, loss: {np.mean(epoch_losses)}')
             print(f'sf: {scale_factor}, tx: {tx}, ty: {ty}')
             # place patch in one image and save
@@ -283,9 +294,8 @@ if __name__ == '__main__':
             # cv2.rectangle(mod_img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 10)
             xmin, ymin, xmax, ymax = selected_boxes[0].detach().cpu().numpy()
             cv2.rectangle(mod_img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 3)
-            cv2.drawMarker(mod_img, (target_x, target_y), (0, 0, 255), markerType=cv2.MARKER_CROSS, markerSize=5, thickness=3)
+            cv2.drawMarker(mod_img, (img_x, img_y), (0, 0, 255), markerType=cv2.MARKER_CROSS, markerSize=5, thickness=3)
             
-            cv2.imwrite(f'misc/dji/temp_batch_prediction/patch_{epoch}.jpg', mod_img)
+            cv2.imwrite(f'{output_folder}/patch_{epoch}.jpg', mod_img)
         # plt.imsave(f'misc/dji/temp_batch_prediction/patch_{epoch}.jpg', mod_img[0].permute(1, 2, 0).detach().cpu().numpy())
-    np.save('misc/dji/optim_patch.npy', best_patch)
-    np.save('misc/dji/optim_transformation.npy', best_position)
+    
